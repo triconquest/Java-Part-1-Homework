7b6706a2b292a337fcc42dcbaca82489f4e12c1f
joe <improvejusto@gmail.com>
2025-10-21 22:09:01 +0400

    Added an external library

--- src/main/java/com/example/hw/Main.java
+++ src/main/java/com/example/hw/Main.java
@@ -1,7 +1,21 @@
 package com.example.hw;
 
+import com.google.gson.Gson;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java Part 1 Homework");
+        Car car = new Car("Ford", 2021);
+        Gson gson = new Gson();
+        System.out.println(gson.toJson(car));
+    }
+
+    static class Car {
+        String brand;
+        int year;
+        Car(String brand, int year)
+        {
+            this.brand = brand;
+            this.year = year;
+        }
     }
 }
