58cb05407039f31fce17101ee847c0cb26a63f4b
joe <improvejusto@gmail.com>
2025-10-21 22:36:41 +0400

    Enriched the previous branch with different types of classes: + inner/nested + anonymous + local classes

--- src/main/java/com/example/hw/Main.java
+++ src/main/java/com/example/hw/Main.java
@@ -4,11 +4,37 @@ import com.google.gson.Gson;
 
 public class Main {
     public static void main(String[] args) {
+        // static nested class example
         Car car = new Car("Ford", 2021);
         Gson gson = new Gson();
-        System.out.println(gson.toJson(car));
+
+        System.out.println("Static nested class JSON: " + gson.toJson(car));
+
+        // inner class
+        Car.Engine engine = car.new Engine("V8");
+        System.out.println("Inner class:  Engine type - " + engine.getType());
+
+        // local class example
+        class CarChecker{
+            void Inspect(Car c)
+            {
+                System.out.println("Checking car: " + c.brand + " (" + c.year + ")");
+            }
+        }
+        CarChecker checker = new CarChecker();
+        checker.Inspect(car);
+
+        // anonymous class example
+        Vehicle electricVehicle = new Vehicle() {
+            @Override
+            public void Drive() {
+                System.out.println("Driving an electric vehicle!");
+            }
+        };
+        electricVehicle.Drive();
     }
 
+    // static nested class
     static class Car {
         String brand;
         int year;
@@ -17,5 +43,23 @@ public class Main {
             this.brand = brand;
             this.year = year;
         }
+
+        // non-static inner class
+        class Engine
+        {
+            private String type;
+
+            Engine(String type) { this.type = type; }
+
+            public String getType()
+            {
+                return type;
+            }
+        }
+    }
+
+    interface Vehicle
+    {
+        void Drive();
     }
 }
